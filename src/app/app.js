1a7ccc3867d764ff4b2ed47b81a241353b91c75f
Eric Correia <ehcorreia@hkmconsultingllc.com>
2016-01-26 11:36:31 -0500

    able to now search local directory for import of swagger document

--- src/app/app.js
+++ src/app/app.js
@@ -3,18 +3,28 @@ import Common from './common/common';
 import Components from './components/components';
 import appComponent from './app.component';
 import angular_materialize from 'angular-materialize';
-import angularFileSaver from 'angular-file-saver'
+import angularFileSaver from 'angular-file-saver';
+import AngularMaterial from 'angular-material';
+import AngularAnimate from 'angular-animate';
+import AngularAria from 'angular-aria';
+import 'angular-material/angular-material.css';
 
 'use strict';
 
-console.log(Components);
+console.log(AngularMaterial);
 
 angular.module("SwaggerGraphicalEditor", [
     // 'ui.materialize',
     angular_materialize,
     'ngFileSaver',
     Common.name,
-    Components.name
+    Components.name,
+    AngularMaterial,
+    AngularAnimate,
+    AngularAria,
+    // 'ngMaterial',
+    // 'ngAnimate',
+    // 'ngAria'
   ])
 
   .directive('app', appComponent);
