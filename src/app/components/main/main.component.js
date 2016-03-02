955a3112b70b26cbbc16ca0314b650d09e3446fe
EricHenry <correia.eh@gmail.com>
2016-03-01 20:07:11 -0500

    first phase of UI change of definition

--- src/app/components/main/main.component.js
+++ src/app/components/main/main.component.js
@@ -2,7 +2,7 @@
  */
 export default {
     template: [
-        "<main>",
+        "<main style='display: flex; flex-flow: column nowrap; max-width: 60%;'>",
         "<sg-info></sg-info>",
         "<sg-path></sg-path>",
         "<sg-definitions></sg-defintions>",
