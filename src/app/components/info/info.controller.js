378047d8bf6c57bbd4828c307abed780b2ebc139
EricHenry <correia.eh@gmail.com>
2016-03-17 15:58:05 -0400

    reduced the number of mimetypes and added ability to create custom ones

--- src/app/components/info/info.controller.js
+++ src/app/components/info/info.controller.js
@@ -6,6 +6,15 @@ export default infoCtrl;
  */
 function InfoCtrl(InfoService) {
 
+    this.state = {
+        consume: null,
+        customConsume: null,
+        produce: null,
+        customProduce: null,
+        schema: null,
+        customSchema: null
+    };
+
     /**
       * @name InfoService
       * @desc A singleton module to hold Base definition state and manipulate it"s info.
@@ -20,7 +29,7 @@ function InfoCtrl(InfoService) {
      **/
     this.types = {
         scheme: ["http", "https", "ws", "wws"],
-        mime: ["text/plain; charset=utf-8", "application/json", "application/vnd.github+json", "application/vnd.github.v3+json", "application/vnd.github.v3.raw+json", "application/vnd.github.v3.text+json", "application/vnd.github.v3.html+json", "application/vnd.github.v3.full+json", "application/vnd.github.v3.diff", "application/vnd.github.v3.patch"]
+        mime: ["create custom", "text/plain", "application/json", "text/xml", "application/xml", "application/atom+xml", "application/x-www-form-urlencoded"]
     };
 
     /**
@@ -28,14 +37,17 @@ function InfoCtrl(InfoService) {
       * @desc invokes a call to the InfoService service to handle the logic of adding types.
       * @type {Function}
      **/
-    this.addType = function addEvent(list, type) {
-        // try {
-        //     InfoService.addType(list, type);
-        // } catch (e) {
-        //     $log.log(e);
-        //     UtilitiesService.toast(e, 3000);
-        // }
+    this.addType = function addEvent(...args) {
+
+        const list = args[0];
+        const type = args[1];
+        const resetDropdown = args[2];
+
         InfoService.addType(list, type);
+
+        if (resetDropdown) {
+            reset.call(this, list);
+        }
     };
 
     /**
@@ -44,13 +56,32 @@ function InfoCtrl(InfoService) {
       * @type {Function}
      **/
     this.removeType = function removeEvent(list, type) {
-        // try {
-        //     InfoService.removeType(list, type);
-        // } catch (e) {
-        //     $log.log(e);
-        //     UtilitiesService.toast(e, 3000);
-        // }
         InfoService.removeType(list, type);
+
     };
+
+    /**
+     *
+     * @param list
+     */
+    function reset(list) {
+        switch (list) {
+        case "consumes":
+            this.state.consume = null;
+            this.state.customConsume = "";
+            break;
+        case "produces":
+            this.state.produce = null;
+            this.state.customProduce = "";
+            break;
+        case "schemes":
+            this.state.scheme = null;
+            this.state.customScheme = "";
+            break;
+        default:
+            return;
+
+        }
+    }
 
 }
