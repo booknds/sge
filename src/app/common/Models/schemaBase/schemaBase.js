452c36a8ef1d76ab28f3ed6e4525e4c1588f971b
EricHenry <correia.eh@gmail.com>
2016-04-13 17:06:04 -0400

    renamed JsonSchemaBase to schemaBase and wrote a test file to validate the object's functions

---
+++ src/app/common/Models/schemaBase/schemaBase.js
@@ -0,0 +1,98 @@
+import createProperty from '../property/property';
+import { getProperty, getAllProps } from '../utils/helpers';
+
+/**
+ *  SchemaBase - A factory function to create a basic JSON Schema Object in accordance
+ *                to the Swagger / OpenAPI specification. Will be used to compose
+ *                other Swagger / OpenAPI objects.
+ *
+ * @return {Object} - a SchemaBaseObject
+ */
+export default function SchemaBase() {
+  const props = [
+    createProperty('format'),
+    createProperty('collectionFormat'),
+    createProperty('default'),
+    createProperty('maximum'),
+    createProperty('exclusiveMaximum'),
+    createProperty('minimum'),
+    createProperty('exclusiveMinimum'),
+    createProperty('maxLength'),
+    createProperty('minLength'),
+    createProperty('pattern'),
+    createProperty('maxItems'),
+    createProperty('minItems'),
+    createProperty('uniqueItems'),
+    createProperty('enum'),
+    createProperty('multipleOf'),
+  ];
+
+  const stateMethods = {
+    setFormat(value) {
+      getProperty(props, 'format').value = value;
+    },
+
+    setCollectionFormat(value) {
+      getProperty(props, 'collectionFormat').value = value;
+    },
+
+    setDefault(value) {
+      getProperty(props, 'default').value = value;
+    },
+
+    setMaximum(value) {
+      getProperty(props, 'maximum').value = value;
+    },
+
+    setExclusiveMaximum(value) {
+      getProperty(props, 'exclusiveMaximum').value = value;
+    },
+
+    setMinimum(value) {
+      getProperty(props, 'minimum').value = value;
+    },
+
+    setExclusiveMinimum(value) {
+      getProperty(props, 'exclusiveMinimum').value = value;
+    },
+
+    setMaxLength(value) {
+      getProperty(props, 'maxLength').value = value;
+    },
+
+    setMinLength(value) {
+      getProperty(props, 'minLength').value = value;
+    },
+
+    setPattern(value) {
+      getProperty(props, 'pattern').value = value;
+    },
+
+    setMaxItems(value) {
+      getProperty(props, 'maxItems').value = value;
+    },
+
+    setMinItems(value) {
+      getProperty(props, 'minItems').value = value;
+    },
+
+    setUniqueItems(value) {
+      getProperty(props, 'uniqueItems').value = value;
+    },
+
+    setEnum(value) {
+      getProperty(props, 'enum').value = value;
+    },
+
+    setMultipleOf(value) {
+      getProperty(props, 'multipleOf').value = value;
+    },
+  };
+
+  const completeState = Object.assign(
+    getAllProps(props),
+    stateMethods
+  );
+
+  return Object.freeze(completeState);
+}
---
+++ src/app/common/Models/schemaBase/schemaBase.spec.js
@@ -0,0 +1,55 @@
+import test from 'ava';
+import SchemaBase from './schemaBase';
+import Property from '../property/property';
+
+// ----------------------------------------------
+// Set up Tests
+// ----------------------------------------------
+let schemaBase;
+const propsThatShouldBeOnSchemaBaseObject =
+  [
+    'format', 'collectionFormat', 'default',
+    'maximum', 'exclusiveMaximum', 'minimum', 'exclusiveMinimum',
+    'maxLength', 'minLength', 'pattern', 'maxItems', 'minItems',
+    'uniqueItems', 'enum', 'multipleOf',
+  ];
+
+test.beforeEach(() => {
+  schemaBase = SchemaBase();
+});
+
+
+// ----------------------------------------------
+// Tests
+// ----------------------------------------------
+
+test('grab all the properties of the schemaBase', t => {
+  const schemaBaseProps = schemaBase.getAllProps();
+
+  t.true(schemaBaseProps.every(prop => (
+    propsThatShouldBeOnSchemaBaseObject.indexOf(prop.key) > -1
+  )));
+});
+
+test('set any value of the schemaBase properties', t => {
+  t.true(
+    schemaBase
+      .getAllProps()
+      .map(prop => (Property(prop.key, 'Keep your head up')))
+      .every(prop => (prop.value === 'Keep your head up')));
+
+  t.true(schemaBase
+    .getAllProps()
+    .map(prop => (Property(prop.key, 'Keep your head up'))).length === 15);
+});
+
+test('should not be able to modify any properties on the schemaBase object', t => {
+  t.throws(
+    () => (schemaBase.newProperty = 'adding a new Property to the schemaBase Object')()
+  );
+
+  t.throws(
+    () =>
+    (schemaBase.getAllProps = () => ('I should not be able to modify this method'))()
+  );
+});
