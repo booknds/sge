a24b25860977882351124240713e2152ab173e55
EricHenry <correia.eh@gmail.com>
2016-05-09 17:32:40 -0400

    continuing work on InfoView UI

--- src/app/components/InfoView/InfoView.component.js
+++ src/app/components/InfoView/InfoView.component.js
@@ -1,5 +1,8 @@
 import template from './InfoView.html';
+import controller from './InfoView.controller.js';
 
 export default {
   template,
+  controller,
+  controllerAs: 'info',
 };
---
+++ src/app/components/InfoView/InfoView.controller.js
@@ -0,0 +1,19 @@
+export default function InfoViewCtrl() {
+  this.info = {
+    title: 'Access',
+    description: 'Booknds Access API is used to provide CRUD facilities to a Resource definition.',
+    version: '0.1',
+    contact: {
+      email: 'info@hkmconsultingllc.com',
+    },
+    termsOfService: 'http://helloreverb.com/terms/',
+   // license: {
+   //   name: 'Apache 2.0',
+   //   url: 'http://www.apache.org/licenses/LICENSE-2.0.html',
+   //  },
+  };
+
+  this.tosIsLink = function tosIsLink() {
+    return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(this.info.termsOfService);
+  };
+}
